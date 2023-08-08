cf46e4b34c9c8ec07271fce118f94019e5c9d6e4
NicolasCBV <nicolascleiton700e@gmail.com>
2023-08-08 02:05:24 -0300

    fix bugs on refresh token route and routes that throw OTPs

--- user-service/src/infra/storages/cache/contract/OTPHandler.ts
+++ user-service/src/infra/storages/cache/contract/OTPHandler.ts
@@ -2,7 +2,7 @@ import { OTP } from '@src/app/entities/OTP/_OTP';
 import { DefaultHandlerParams } from '../redis/handlers';
 
 export abstract class OTPHandlerContract extends DefaultHandlerParams {
-  abstract sendOTP(otp: OTP, email: string): Promise<void>;
-  abstract getOTP(email: string): Promise<OTP | null>;
+  abstract sendOTP(otp: OTP, email: string, login?: boolean): Promise<void>;
+  abstract getOTP(email: string, login?: boolean): Promise<OTP | null>;
   abstract getCancelKeyOTP(email: string): Promise<OTP | null>;
 }
